fc66a387b97ab20ca0776829d8e4c07abdae0ee3
Satoshi-Yamazaki <moisessteiner.ms@outlook.com>
2025-09-28 18:35:05 -0300

    speedtest - pip modification

--- metrics.py
+++ metrics.py
@@ -14,20 +14,23 @@ def ping_host(host):
     except:
         return None
 
+import speedtest
+import datetime
+
 def medir_speedtest(data_lock, ping_status):
     try:
-        result = subprocess.run(["speedtest", "--json"], capture_output=True, text=True, timeout=60)
-        if result.returncode == 0 and result.stdout:
-            data = json.loads(result.stdout)
-            download = round(data["download"] / 1_000_000, 2)
-            upload = round(data["upload"] / 1_000_000, 2)
-            ping_val = round(data["ping"], 2)
+        s = speedtest.Speedtest()
+        s.get_best_server()
+        download = round(s.download() / 1_000_000, 2)  # Mbps
+        upload = round(s.upload() / 1_000_000, 2)      # Mbps
+        ping_val = round(s.results.ping, 2)
 
-            with data_lock:
-                ping_status["download"] = download
-                ping_status["upload"] = upload
+        with data_lock:
+            ping_status["download"] = download
+            ping_status["upload"] = upload
 
-            save_speedtest(datetime.datetime.now().isoformat(), download, upload, ping_val)
-            print(f"[{datetime.datetime.now().isoformat()}] Speedtest salvo -> D: {download} Mbps, U: {upload} Mbps, Ping: {ping_val} ms")
+        save_speedtest(datetime.datetime.now().isoformat(), download, upload, ping_val)
+        print(f"[{datetime.datetime.now().isoformat()}] Speedtest salvo -> D: {download} Mbps, U: {upload} Mbps, Ping: {ping_val} ms")
+
     except Exception as e:
         print("Erro no speedtest:", e)
